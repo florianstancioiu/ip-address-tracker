9d6862f41b9a30d2f090e10aae43760a11c896aa
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-05-05 07:29:47 +0300

    feat: configure the wds

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,12 +1,8 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
+  open: false,
+  preserveSymlinks: true,
   nodeResolve: true,
-  plugins: [
-    esbuildPlugin({
-      tsx: true,
-      jsxFactory: 'React.createElement',
-      jsxFragment: 'Fragment',
-    }),
-  ],
+  plugins: [esbuildPlugin({ ts: true })],
 };
