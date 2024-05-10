4b40e5b1cb95dff990ea4cf36c3c4491899ea0d1
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-05-10 16:32:54 +0300

    feat: remove files that arent needed in branch

--- web-dev-server.config.mjs
+++
@@ -1,8 +0,0 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
-
-export default {
-  open: false,
-  preserveSymlinks: true,
-  nodeResolve: true,
-  plugins: [esbuildPlugin({ ts: true })],
-};
